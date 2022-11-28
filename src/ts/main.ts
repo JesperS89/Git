bfd5fddc02d79fc1ca10ae70495cd26623a98b22
Jesper <jesperhardin@gmail.com>
2022-11-28 13:39:12 +0100

    hej

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,14 @@
-console.log("hej");
+let input1: HTMLInputElement = document.createElement("input");
+let input2: HTMLInputElement = document.createElement("input");
+let addbutton: HTMLButtonElement = document.createElement("button");
+
+document.body.appendChild(input1);
+document.body.appendChild(input2);
+document.body.appendChild(addbutton);
+
+addbutton.innerHTML = "Addera";
+
+addbutton.addEventListener("click", () => {
+  let sum: number = add(parseInt(input.value), parseInt(input2.value));
+  printHTML(sum);
+});
