d08b8933d1d2ef36fef4f0604096377abcf36c62
Marcus C. Sahlin <mcsahlin@outlook.com>
2022-11-28 13:38:42 +0100

    tessting 12

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,18 @@
 console.log("hej");
+
+function add(x: number, y: number): number {
+  let sum: number = x + y;
+  return sum;
+}
+
+// function createHtml(tag: string) {
+//   // return document.createElement("'" + tag + "'");
+// }
+
+// createHtml("input");
+
+function printHtml(sum: number): void {
+  let heading: HTMLHeadingElement = document.createElement("h3");
+  heading.innerHTML = sum.toString();
+  document.body.appendChild(heading);
+}
